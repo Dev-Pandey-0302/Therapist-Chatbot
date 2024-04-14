afdad855dae664e7b95172a9c18840abdcc02026
Dev-Pandey-0302 <dev.pandey.0302@gmail.com>
2024-04-14 19:18:56 +0530

    FINAL MESSAGE

--- app_docs_upload.py
+++ app_docs_upload.py
@@ -1,18 +1,62 @@
 from openai import OpenAI
 from gtts import gTTS
-from io import BytesIO
+from io import BytesIO,StringIO
 #Using streamlit app framework
 import streamlit as st
+
+
+
 
 #Creating client
 client= OpenAI(
     api_key="sk-1234567890",
     base_url='http://localhost:8000/v1'
 )
+
+
 # Title of the app
 st.title("TherapyBot- A Chatbot for Mental Health Support")
 
-prompt= st.chat_input('How can I help you today?')
+
+
+
+#Uploading medical records
+uploaded_file = st.file_uploader("", type=["txt"], label_visibility="collapsed")
+css = '''
+<style>
+    [data-testid='stFileUploader'] {
+        width: max-content;
+    }
+    [data-testid='stFileUploader'] section {
+        padding: 0;
+        float: left;
+    }
+    [data-testid='stFileUploader'] section > input + div {
+        display: none;
+    }
+    [data-testid='stFileUploader'] section + div {
+        float: right;
+        padding-top: 0;
+    }
+
+</style>
+'''
+st.markdown(css, unsafe_allow_html=True)
+
+doc_data = ""
+#If user uploads a file
+if uploaded_file is not None:
+
+    # To read file as string:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    doc_data = stringio.read()
+    doc_data = "This is my medical record - "+doc_data
+    
+
+
+#Taking user input
+prompt= st.chat_input('Chat to start or upload any medical records. How can I help?')
+
 
 #If users types prompt and hits enter
 if prompt:
@@ -24,7 +68,7 @@ if prompt:
         #pass through the prompt
         messages=[{
             'role': 'user',
-            'content': prompt
+            'content': doc_data+" Please answer the following question based on the earlier medical record- "+prompt
         }],
         #Add streaming
         stream=True
